6d5ad49166e51404f093d9019e2db692ecbc628e
abutha <102573765+thaabu@users.noreply.github.com>
2022-04-19 11:18:42 +0530

    Update abu.js

--- abu.js
+++ abu.js
@@ -1,6 +1,5 @@
 
-/* codded by afnanplk
-modified by Afx-Abu
+/* codded by Abuser
 */
 
 const { Sequelize } = require('sequelize');
@@ -15,6 +14,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
  Bot: process.env.Bot === undefined ? 'https://avatars.githubusercontent.com/u/85664936?s=1080&v=1080.png' : process.env.Bot,
  THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/xsKCqwZ.jpeg' : process.env.THUMBNAIL_URL,
- VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[ABU SER]\nCoded by @AFX-ABU' : process.env.VERIFICATION_CAPTION,
+ VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[ABU SER]\nCoded by @ABU-SER' : process.env.VERIFICATION_CAPTION,
     
 };
